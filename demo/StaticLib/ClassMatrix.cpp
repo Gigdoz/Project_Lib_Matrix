0a840137117b2ac96d0a5d3f9725e1385c943907
Dydin Danil <dydlver123@gmail.com>
2023-03-29 22:31:21 +0500

    Separate functions were created to catch exceptions.

--- demo/StaticLib/ClassMatrix.cpp
+++ demo/StaticLib/ClassMatrix.cpp
@@ -3,6 +3,39 @@
 
 using namespace std;
 
+#pragma region exceptions
+
+void __throw_if_out_of_range(Matrix* m, UINT row, UINT col)
+{
+	if (row >= m->GetRows() || col >= m->GetCols())
+	{
+		throw std::out_of_range("Out of range!");
+	}
+}
+void __throw_if_matrices_size_does_not_match(const Matrix* a, const Matrix* b)
+{
+	if (a->GetRows() != b->GetRows() || a->GetCols() != b->GetCols())
+		throw std::invalid_argument("The size of the matrices does not match!");
+}
+
+void __throw_if_matrices_rows_and_columns_does_not_match(const Matrix* a, const Matrix* b)
+{
+	if (b->GetRows() != a->GetCols())
+		throw std::invalid_argument("The rows and columns of the matrices does not match!");
+}
+
+void __throw_if_matrix_does_not_exist(float** matrix_array)
+{
+	if (!matrix_array)
+		throw std::invalid_argument("The matrix don't exist!");
+}
+void __throw_if_non_square_matrix(Matrix* m)
+{
+	if (m->GetRows() != m->GetCols())
+		throw std::invalid_argument(" The matrix is not square!");
+}
+#pragma endregion
+
 Matrix::Matrix(const Matrix &otherMatrix) // Constructor copying
 {
 	this->SetSizeMatrix(otherMatrix.rows, otherMatrix.cols);
@@ -32,240 +65,134 @@ void Matrix::SetSizeMatrix(const UINT rows, const UINT cols)
 
 void Matrix::InputElementMatrix(const float value, UINT row, UINT col)
 {
-	if (row < rows && col < cols)
-		 matrix[row][col] = value;
-	else
-	{
-		cout << "Out of range!" << endl;
-		throw 1;
-	}
+	__throw_if_out_of_range(this, row, col);
+	matrix[row][col] = value;
 }
 
 float Matrix::OutputElement(const UINT row, const UINT col)
 {
-	if (row < rows && col < cols)
-		return matrix[row][col];
-	else
-	{
-		cout << "Out of range!" << endl;
-		throw 1;
-	}
+	__throw_if_out_of_range(this, row, col);
+	return matrix[row][col];
 }
 
 Matrix Matrix::operator+(const Matrix &otherMatrix)
 {
-	if (otherMatrix.matrix)
-	{
-		if (this->rows == otherMatrix.rows && this->cols == otherMatrix.cols)
+	__throw_if_matrix_does_not_exist(otherMatrix.matrix);
+	__throw_if_matrices_size_does_not_match(this, &otherMatrix);
+	Matrix temp;
+	temp.SetSizeMatrix(otherMatrix.rows, otherMatrix.cols);
+	for (UINT i = 0; i < temp.rows; i++)
+		for (UINT j = 0; j < temp.cols; j++)
 		{
-			Matrix temp;
-			temp.SetSizeMatrix(otherMatrix.rows, otherMatrix.cols);
-			for (UINT i = 0; i < temp.rows; i++)
-				for (UINT j = 0; j < temp.cols; j++)
-				{
-					temp.matrix[i][j] = this->matrix[i][j] + otherMatrix.matrix[i][j];
+			temp.matrix[i][j] = this->matrix[i][j] + otherMatrix.matrix[i][j];
 
-				}
-			return temp;
 		}
-		else
-		{
-			cout << "The size of the matrices does not match!" << endl;
-			throw 1;
-		}
-	}
-	else
-	{
-		cout << "The matrix don't exist!" << endl;
-		throw 1;
-	}
+	return temp;
 }
 
 Matrix Matrix::operator-(const Matrix &otherMatrix)
 {
-	if (otherMatrix.matrix)
-	{
-		if (this->rows == otherMatrix.rows && this->cols == otherMatrix.cols)
+	__throw_if_matrix_does_not_exist(otherMatrix.matrix);
+	__throw_if_matrices_size_does_not_match(this, &otherMatrix);
+	Matrix temp;
+	temp.SetSizeMatrix(otherMatrix.rows, otherMatrix.cols);
+	for (UINT i = 0; i < temp.rows; i++)
+		for (UINT j = 0; j < temp.cols; j++)
 		{
-			Matrix temp;
-			temp.SetSizeMatrix(otherMatrix.rows, otherMatrix.cols);
-			for (UINT i = 0; i < temp.rows; i++)
-				for (UINT j = 0; j < temp.cols; j++)
-				{
-					temp.matrix[i][j] = this->matrix[i][j] - otherMatrix.matrix[i][j];
+			temp.matrix[i][j] = this->matrix[i][j] - otherMatrix.matrix[i][j];
 
-				}
-			return temp;
 		}
-		else
-		{
-			cout << "The size of the matrices does not match!" << endl;
-			throw 1;
-		}
-	}
-	else
-	{
-		cout << "The matrix don't exist!" << endl;
-		throw 1;
-	}
-	
+	return temp;
 }
 
 Matrix Matrix::operator*(const Matrix &otherMatrix)
 {
-	if (otherMatrix.matrix)
-	{
-		
-		if (this->cols == otherMatrix.rows)
+	__throw_if_matrix_does_not_exist(otherMatrix.matrix);
+	__throw_if_matrices_rows_and_columns_does_not_match(this, &otherMatrix);
+	Matrix temp;
+	UINT k, j, i;
+	temp.SetSizeMatrix(rows, otherMatrix.cols);
+	for (k = 0; k < temp.rows; k++)
+		for (j = 0; j < temp.cols; j++)
 		{
-			Matrix temp;
-			UINT k, j, i;
-			temp.SetSizeMatrix(rows, otherMatrix.cols);
-			for (k = 0; k < temp.rows; k++)
-				for (j = 0; j < temp.cols; j++)
-				{
-					temp.matrix[k][j] = 0;
-					for (i = 0; i < cols; i++)
-						temp.matrix[k][j] += this->matrix[k][i] * otherMatrix.matrix[i][j];
-				}
-			return temp;
+			temp.matrix[k][j] = 0;
+			for (i = 0; i < cols; i++)
+				temp.matrix[k][j] += this->matrix[k][i] * otherMatrix.matrix[i][j];
 		}
-		else
-		{
-			cout << "The size of the matrices does not match!" << endl;
-			throw 1;
-		}
-	}
-	else
-	{
-		cout << "The matrix don't exist!" << endl;
-		throw 1;
-	}
+	return temp;
 }
 
 Matrix Matrix::operator*(float s)
 {
-	if (this->matrix)
-	{
-		Matrix temp;
-		temp.SetSizeMatrix(rows, cols);
-		for (UINT i = 0; i < temp.rows; i++)
-			for (UINT j = 0; j < temp.cols; j++)
-			{
-				temp.matrix[i][j] = s * this->matrix[i][j];
-			}
-		return temp;
-	}
-	else
-	{
-		cout << "The matrix don't exist!" << endl;
-		throw 1;
-	}
+	__throw_if_matrix_does_not_exist(this->matrix);
+	Matrix temp;
+	temp.SetSizeMatrix(rows, cols);
+	for (UINT i = 0; i < temp.rows; i++)
+		for (UINT j = 0; j < temp.cols; j++)
+		{
+			temp.matrix[i][j] = s * this->matrix[i][j];
+		}
+	return temp;
 }
 
 Matrix& Matrix::operator=(const Matrix &otherMatrix)
 {
-	if (otherMatrix.matrix)
-	{
-		this->SetSizeMatrix(otherMatrix.rows, otherMatrix.cols);
-		for (UINT i = 0; i < this->rows; i++)
-			for (UINT j = 0; j < this->cols; j++)
-			{
-				this->matrix[i][j] = otherMatrix.matrix[i][j];
-			}
-		return *this;
-	}
-	else
-	{
-		cout << "It don't exist!" << endl;
-		throw 1;
-	}
+	__throw_if_matrix_does_not_exist(otherMatrix.matrix);
+	this->SetSizeMatrix(otherMatrix.rows, otherMatrix.cols);
+	for (UINT i = 0; i < this->rows; i++)
+		for (UINT j = 0; j < this->cols; j++)
+		{
+			this->matrix[i][j] = otherMatrix.matrix[i][j];
+		}
+	return *this;
 }
 
 Matrix& Matrix::operator+=(const Matrix &otherMatrix)
 {
-	if (otherMatrix.matrix)
-	{
-		if (this->rows == otherMatrix.rows && this->cols == otherMatrix.cols)
+	__throw_if_matrix_does_not_exist(otherMatrix.matrix);
+	__throw_if_matrices_size_does_not_match(this, &otherMatrix);
+	for (UINT i = 0; i < this->rows; i++)
+		for (UINT j = 0; j < this->cols; j++)
 		{
-			for (UINT i = 0; i < this->rows; i++)
-				for (UINT j = 0; j < this->cols; j++)
-				{
-					this->matrix[i][j] = this->matrix[i][j] + otherMatrix.matrix[i][j];
-
-				}
-			return *this;
+			this->matrix[i][j] = this->matrix[i][j] + otherMatrix.matrix[i][j];
 		}
-		else
-		{
-			cout << "The size of the matrices does not match!" << endl;
-			throw 1;
-		}
-	}
-	else
-	{
-		cout << "The matrix don't exist!" << endl;
-		throw 1;
-	}
+	return *this;
 }
 
 bool Matrix::operator==(const Matrix &otherMatrix)
 {
-	if (otherMatrix.matrix)
-	{
-		if (this->rows == otherMatrix.rows && this->cols == otherMatrix.cols)
+	__throw_if_matrix_does_not_exist(otherMatrix.matrix);
+	__throw_if_matrices_size_does_not_match(this, &otherMatrix);
+	for (UINT i = 0; i < this->rows; i++)
+		for (UINT j = 0; j < this->cols; j++)
 		{
-
-			for (UINT i = 0; i < this->rows; i++)
-				for (UINT j = 0; j < this->cols; j++)
-				{
-					if(this->matrix[i][j] != otherMatrix.matrix[i][j]) return false;
-				}
-			return true;
+			if(this->matrix[i][j] != otherMatrix.matrix[i][j]) return false;
 		}
-		else
-		{
-			cout << "The size of the matrices does not match!" << endl;
-			return false;
-		}
-	}
-	else
-	{
-		cout << "The matrix don't exist!" << endl;
-		throw 1;
-	}
+	return true;
 }
 
 Matrix Matrix::Transposition_Matrix()
 {
 	
-	if (matrix)
-	{
-		Matrix NewMatrix(cols, rows);
-		for (UINT i = 0; i < rows; i++)
-			for (UINT j = 0; j < cols; j++)
-			{
-				NewMatrix.matrix[j][i] = matrix[i][j];
-			}
-		Matrix temp(cols, rows);
-		for (UINT i = 0; i < rows; i++)
-			for (UINT j = 0; j < cols; j++)
-			{
-				 temp.matrix[j][i] =  matrix[i][j];
-			}
-		for (UINT i = 0; i < cols; i++)
-			for (UINT j = 0; j < rows; j++)
-			{
-				NewMatrix.matrix[i][j] = temp.matrix[i][j];
-			}
-		return NewMatrix;
-
-	}
-	else
-	{
-		cout << "The matrix don't exist!" << endl;
-		throw 1;
-	}
+	__throw_if_matrix_does_not_exist(this->matrix);
+	Matrix NewMatrix(cols, rows);
+	for (UINT i = 0; i < rows; i++)
+		for (UINT j = 0; j < cols; j++)
+		{
+			NewMatrix.matrix[j][i] = matrix[i][j];
+		}
+	Matrix temp(cols, rows);
+	for (UINT i = 0; i < rows; i++)
+		for (UINT j = 0; j < cols; j++)
+		{
+			 temp.matrix[j][i] =  matrix[i][j];
+		}
+	for (UINT i = 0; i < cols; i++)
+		for (UINT j = 0; j < rows; j++)
+		{
+			NewMatrix.matrix[i][j] = temp.matrix[i][j];
+		}
+	return NewMatrix;
 }
 
 void getMatrixMinor(Matrix &other, UINT row, UINT col, Matrix &NewMatrix)
@@ -292,26 +219,19 @@ void getMatrixMinor(Matrix &other, UINT row, UINT col, Matrix &NewMatrix)
 }
 
 float Det(Matrix &other) {
-	if (other.rows == other.cols)
-	{
-		int det = 0;
-		if (other.rows == 1) return other.matrix[0][0];
-		if (other.rows == 2) return other.matrix[0][0] * other.matrix[1][1] - other.matrix[0][1] * other.matrix[1][0];
-		else
-		{
-			Matrix NewMatrix;
-			NewMatrix.SetSizeMatrix(other.rows - 1, other.rows - 1);
-			for (UINT j = 0; j < other.rows; j++)
-			{
-				getMatrixMinor(other, 0, j, NewMatrix);
-				det += pow(-1, j) * other.matrix[0][j] * Det(NewMatrix);
-			}
-		}
-		return det;
-	}
+	__throw_if_non_square_matrix(&other);
+	int det = 0;
+	if (other.rows == 1) return other.matrix[0][0];
+	if (other.rows == 2) return other.matrix[0][0] * other.matrix[1][1] - other.matrix[0][1] * other.matrix[1][0];
 	else
 	{
-		cout << " The matrix is not square!";
-		throw 1;
+		Matrix NewMatrix;
+		NewMatrix.SetSizeMatrix(other.rows - 1, other.rows - 1);
+		for (UINT j = 0; j < other.rows; j++)
+		{
+			getMatrixMinor(other, 0, j, NewMatrix);
+			det += pow(-1, j) * other.matrix[0][j] * Det(NewMatrix);
+		}
 	}
+	return det;
 }
