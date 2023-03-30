57eed084dee3a11e633d14a4183415f2d8567f02
Dydin Danil <dydlver123@gmail.com>
2023-03-30 19:50:49 +0500

    The "Setsize" method has been moved from public to private

--- demo/StaticLib/Matrix.h
+++ demo/StaticLib/Matrix.h
@@ -9,19 +9,20 @@ class Matrix
 private:
 	float** matrix;
 	UINT rows, cols;
+	void SetSizeMatrix(UINT rows, UINT cols);
 public:
 
 	Matrix() { SetSizeMatrix(1, 1); } // Default Constructor
 	Matrix(UINT rows,  UINT cols) { SetSizeMatrix(rows, cols); } // Constructor
 	Matrix(const Matrix &other); // Constructor copying
-	void SetSizeMatrix(UINT rows, UINT cols);
+	
 	UINT GetRows() const { return rows; }
 	UINT GetCols() const { return cols; }
 	const float Det();
 	void getMatrixMinor(UINT row, UINT col, Matrix &NewMatrix);
 	float& operator()(UINT row, UINT col); // The matrix element input
 	float operator()(UINT row, UINT col) const; // The matrix element output 
-	Matrix Transposed_matrix(); // Transposition matrix
+	Matrix Transposed_matrix(); // Transpose of a matrix
 	Matrix operator+(const Matrix &otherMatrix);
 	Matrix operator-(const Matrix &otherMatrix);
 	Matrix operator*(const Matrix &otherMatrix);
